a1dbe99f7e6d5aab6cc3b48de68b864903af2872
Awiteb <59842932+Awiteb@users.noreply.github.com>
2021-07-06 02:56:54 +0300

    edit db.row

--- db/row.py
+++ db/row.py
@@ -1,6 +1,6 @@
 from .db_config import (cursor, lock)
 
-def row(table_name:str, column:str, word:str, want='*'):
+def row(table_name:str, column:str, word:str, want='*', lst=True):
     """ جلب صف من قاعدة البيانات
 
     المتغيرات:
@@ -8,21 +8,28 @@ def row(table_name:str, column:str, word:str, want='*'):
         column (str): اسم العمود اذي يوجد به الصف
         word (str): القيمة الموجود في العمود
         want (str, optional): word العمود الذي تريده من الصف الذي يوجد به العمود الي قيمته. Defaults to '*'.
+        lst (bool, optional): اخراج المعطيات كلستة ام تيبل. Defaults to 'True'.
 
     المخرجات:
-        [list,str,None]: قائمة بالنتائج او عنصر او لاشي اذ لم تكن هناك نتائج
+        [list,tuple,str,None]: قائمة بالنتائج او عنصر او لاشي اذ لم تكن هناك نتائج
     """
     try:
         lock.acquire(True)
         cursor.execute(f"SELECT {want} FROM {table_name} WHERE {column}='{word}'")
-        result = list(map(
-                    lambda val: str(val).replace('<br>', '\n'),
-                        [val for table in cursor.fetchall() for val in table]
-                        ))
+        if lst:
+            result = list(map(
+                        lambda val: str(val).replace('<br>', '\n'),
+                            [val for t in cursor.fetchall() for val in t]
+                            ))
+        else:
+            result = list(map(
+                        lambda t: tuple(str(val) for val in t),
+                            [t for t in cursor.fetchall()]
+            ))
         if (len(result) == 0):
             return None
         elif (len(result) == 1):
-            return result[0]
+            return result[0] if lst else result[0][0]
         else:
             return result
     finally:
