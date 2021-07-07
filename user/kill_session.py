11c8d0a6cc254d119d3dfe84a6098c4306e8a5f0
Awiteb <59842932+Awiteb@users.noreply.github.com>
2021-07-07 05:02:31 +0300

    Clear session messages from the session, and delete session messages IDs from the database

--- user/kill_session.py
+++ user/kill_session.py
@@ -1,4 +1,5 @@
 import db
+from .del_session_message import del_session_message
 
 def kill_session(session_id:str):
     """ اغلاق الجلسة
@@ -6,4 +7,6 @@ def kill_session(session_id:str):
     Args:
         session_id (str): ايدي الجلسة
     """
+    del_session_message(session_id)
+    db.del_row('sessions_messages', 'session', session_id)
     db.del_row('chat_sessions', 'sessions', session_id)
