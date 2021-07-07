26bdd5f58def97334aedc8db7fdbae754b239a8f
Awiteb <59842932+Awiteb@users.noreply.github.com>
2021-07-07 04:58:32 +0300

    edit: Return message id of message

--- sender/send_message.py
+++ sender/send_message.py
@@ -3,40 +3,44 @@ from config import bot
 def send_message(chat_id, replyType, reply, text, markup=None, msg_id=None):
     """ ارسال رسالة الى مستخدم
 
-    Args:
+    المتغيرات:
         chat_id (str): المستخدم المراد ارسال الرسالة له
         replyType (str): نوع الرسالة
         reply (str): النص، او ايدي الملف المراد ارساله
         text (str): نص الرسالة او ال شرح الملف
         markup (telebot.types.InlineKeyboardMarkup, optional): الازرار. Defaults to None.
         msg_id (str,int, optional): ايدي الرسالة لعمل ربلي لها. Defaults to None.
+    المخرجات:
+        [int]: ايدي الرسالة التي تم ارسالها
     """
+    
     if replyType == "text":
-        bot.send_message(chat_id=chat_id, text=reply, reply_to_message_id=msg_id,
+        msg = bot.send_message(chat_id=chat_id, text=reply, reply_to_message_id=msg_id,
                             reply_markup=markup, )
     elif replyType == "audio":
-        bot.send_audio(chat_id=chat_id, audio=reply, caption=text if text != 'None' else "",
+        msg = bot.send_audio(chat_id=chat_id, audio=reply, caption=text if text != 'None' else "",
                         reply_to_message_id=msg_id, reply_markup=markup, )
     elif replyType == "document":
-        bot.send_document(chat_id=chat_id, data=reply, caption=text if text != 'None' else "",
+        msg = bot.send_document(chat_id=chat_id, data=reply, caption=text if text != 'None' else "",
                         reply_to_message_id=msg_id, reply_markup=markup, )
     elif replyType == "photo":
-        bot.send_photo(chat_id=chat_id, photo=reply, caption=text if text != 'None' else "",
+        msg = bot.send_photo(chat_id=chat_id, photo=reply, caption=text if text != 'None' else "",
                         reply_to_message_id=msg_id, reply_markup=markup, )
     elif replyType == "sticker":
-        bot.send_sticker(chat_id=chat_id, data=reply,
+        msg = bot.send_sticker(chat_id=chat_id, data=reply,
                             reply_to_message_id=msg_id, reply_markup=markup)
     elif replyType == "video":
-        bot.send_video(chat_id=chat_id, data=reply, caption=text if text != 'None' else "",
+        msg = bot.send_video(chat_id=chat_id, data=reply, caption=text if text != 'None' else "",
                         reply_to_message_id=msg_id, reply_markup=markup, )
     elif replyType == "video_note":
-        bot.send_video_note(chat_id=chat_id, data=reply,
+        msg = bot.send_video_note(chat_id=chat_id, data=reply,
                         reply_to_message_id=msg_id, reply_markup=markup, )
     elif replyType == "voice":
-        bot.send_voice(chat_id=chat_id, voice=reply, caption=text if text != 'None' else "",
+        msg = bot.send_voice(chat_id=chat_id, voice=reply, caption=text if text != 'None' else "",
                         reply_to_message_id=msg_id, reply_markup=markup, )
     elif replyType == "animation":
-        bot.send_animation(chat_id=chat_id, animation=reply, caption=text, reply_to_message_id=msg_id,
+        msg = bot.send_animation(chat_id=chat_id, animation=reply, caption=text, reply_to_message_id=msg_id,
                             reply_markup=markup)
     else:
-        pass
+        return
+    return msg.id
