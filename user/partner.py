ff2c735f5c8374dbe4663dd6153817b9c51074c8
Awiteb <59842932+Awiteb@users.noreply.github.com>
2021-07-08 22:17:23 +0300

    update funcions

--- user/partner.py
+++ user/partner.py
@@ -1,5 +1,6 @@
 import db
 from .get_sessions import get_sessions
+from .in_sessions import in_sessions
 
 def partner(user_id:str):
     """ جلب شريك الجلسة الخاصة ب المستخدم
@@ -10,9 +11,12 @@ def partner(user_id:str):
     المخرجات:
         [str,None]: ايدي الشريك او لاشي
     """
-    session_id = get_sessions(user_id)
-    partner_id = ''.join(list(filter(
-            lambda u_id: u_id!=user_id,
-                db.row('chat_sessions', 'sessions', session_id, 'user_id')
-    )))
-    return partner_id
+    if in_sessions(user_id):
+        session_id = get_sessions(user_id)
+        partner_id = ''.join(list(filter(
+                lambda u_id: u_id!=user_id,
+                    db.row('chat_sessions', 'sessions', session_id, 'user_id')
+        )))
+        return partner_id
+    else:
+        return None
--- user/save_username.py
+++ user/save_username.py
@@ -1,6 +1,7 @@
 import db
 from telebot.types import Message
 from config import bot
+from time import time
 
 def save_username(message:Message, user_id:str, new_user:bool):
     """ اضافة شخص الى قاعدة البيانات او تعديل اسمه
@@ -21,13 +22,17 @@ def save_username(message:Message, user_id:str, new_user:bool):
                 msg = bot.reply_to(message, "للالغاء ارسل: /cancel\nالاسم المستعار يجب ان لا يبدا ب / \nحاول مجددا")
                 bot.register_next_step_handler(msg, save_username, user_id, new_user)
         else:
-            # اذا المستخدم جديد يتم اضافته الى قاعدة البيانات
-            if new_user:
-                db.insert('users', (user_id, username))
-            # اذا قديم يتم تحديث اسمه
+            if len(username) <=10:
+                # اذا المستخدم جديد يتم اضافته الى قاعدة البيانات
+                if new_user:
+                    db.insert('users', (user_id, username, time(), 0, ""))
+                # اذا قديم يتم تحديث اسمه
+                else:
+                    db.update("users", 'username', username, 'id', user_id)
+                bot.reply_to(message, "تم تحديث الاسم بنجاح الى '{}'".format(username))
             else:
-                db.update("users", 'username', username, 'id', user_id)
-            bot.reply_to(message, "تم تحديث الاسم بنجاح الى '{}'".format(username))
+                msg = bot.reply_to(message, "للالغاء ارسل: /cancel\nيجب ان لا يتخطى طول الاسم 10 خانات \nحاول مجددا")
+                bot.register_next_step_handler(msg, save_username, user_id, new_user)
     else:
         msg = bot.reply_to(message, "للالغاء ارسل: /cancel\nالاسم المستعار يجب ان يكون نص\nحاول مجددا")
         bot.register_next_step_handler(msg, save_username, user_id, new_user)
