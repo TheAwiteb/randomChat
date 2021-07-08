4524a2e44c62ea80f98cdbedec19da750e7746a0
Awiteb <59842932+Awiteb@users.noreply.github.com>
2021-07-08 05:08:17 +0300

    modified: add message id to reply

--- sender/send_to_partner.py
+++ sender/send_to_partner.py
@@ -5,17 +5,18 @@ import db
 import user
 import markup
 
-def send_to_partner(message:Message, chat_id:str):
-    """ ارسال الرسالة الى رفيق الشخص
+def send_to_partner(message:Message, chat_id:str, rep_msg_id=None):
+    """" ارسال الرسالة الى رفيق الشخص
 
-    Args:
+    المتغيرات:
         message (telebot.types.Message): الرسالة
         chat_id (str): الشخص الذي يريد ارسالها
+        rep_msg_id (str, optional): ايدي الرسالة المراد الرد عليها. Defaults to None.
     """
     reply, replyType, text = get_reply(message)
     partner_id = user.partner(chat_id)
     session_id = user.get_sessions(chat_id)
     msg_id = message.id
     partner_msg_id = send_message(partner_id, replyType, reply, text,
-                    markup.username(chat_id), msg_id=None)
+                    markup.username(chat_id), msg_id=rep_msg_id)
     db.insert("sessions_messages", [session_id, chat_id, msg_id, partner_msg_id])
